8fa2aa83087812dc0cd4f169a572c1e90669c3cd
abidTss <abid.teastallstudio@gmail.com>
2017-03-27 16:59:44 +0530

    7364783ryde

---
+++ bannerautoscroll/src/main/java/com/example/bannerautoscroll/AutoScrollPagerAdapter.java
@@ -0,0 +1,67 @@
+package com.example.bannerautoscroll;
+
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.util.ArrayList;
+
+
+public class AutoScrollPagerAdapter extends PagerAdapter {
+
+    private ArrayList<String> picList;
+    private Context context;
+    private LayoutInflater inflater;
+
+    public AutoScrollPagerAdapter(Context context, ArrayList<String> picList) {
+
+        this.picList = picList;
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+    }
+
+    public AutoScrollPagerAdapter(Context context) {
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+    }
+
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((LinearLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        LinearLayout linearLayout=new LinearLayout(context);
+        LinearLayout.LayoutParams layoutParams=new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        linearLayout.setLayoutParams(layoutParams);
+        ImageView imageView=new ImageView(context);
+        imageView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        imageView.setImageDrawable(context.getResources().getDrawable(R.drawable.images));
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        linearLayout.addView(imageView);
+        View itemView = inflater.inflate((XmlPullParser) linearLayout, container, false);
+
+        container.addView(itemView);
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
---
+++ bannerautoscroll/src/main/java/com/example/bannerautoscroll/HealperC.java
@@ -0,0 +1,94 @@
+package com.example.bannerautoscroll;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.view.ViewPager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/**
+ * Created by android-1 on 24/3/17.
+ */
+
+public class HealperC {
+    Context context;
+    int dotsCount = 0;
+    AutoScrollPagerAdapter autoScrollPagerAdapter;
+    AutoScrollViewPager autoScrollViewPager;
+    private ImageView[] dots;
+    LinearLayout dotsLinearLayout;
+
+    HealperC(Context context, AutoScrollViewPager autoScrollViewPager, int dotsCount, LinearLayout dotsLinearLayout) {
+        this.context = context;
+        this.autoScrollViewPager = autoScrollViewPager;
+        this.dotsLinearLayout = dotsLinearLayout;
+        this.dotsCount = dotsCount;
+    }
+
+    public void setupBanner() {
+        autoScrollPagerAdapter = new AutoScrollPagerAdapter(context);
+        autoScrollViewPager.setAdapter(autoScrollPagerAdapter);
+        setUiPageViewController();
+        autoScrollViewPager.setOnPageChangeListener(getPageListyener());
+    }
+
+    public AutoScrollViewPager.OnPageChangeListener getPageListyener() {
+        return onPageChangeListener;
+    }
+
+    AutoScrollViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            for (int i = 0; i < dotsCount; i++) {
+                if (Build.VERSION.SDK_INT <= 21)
+                    dots[i].setImageDrawable(context.getResources().getDrawable(R.drawable.normal_circle_item));
+                else
+                    dots[i].setImageDrawable(context.getResources().getDrawable(R.drawable.normal_circle_item, null));
+            }
+            if (Build.VERSION.SDK_INT <= 21) {
+
+                dots[position].setImageDrawable(context.getResources().getDrawable(R.drawable.selected_circle_item));
+            } else {
+                dots[position].setImageDrawable(context.getResources().getDrawable(R.drawable.selected_circle_item, null));
+            }
+
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+
+    };
+
+    public void setUiPageViewController() {
+        dots = new ImageView[dotsCount];
+        dotsLinearLayout.removeAllViews();
+
+        for (int i = 0; i < dotsCount; i++) {
+            dots[i] = new ImageView(context);
+            dots[i].setPadding(5, 0, 5, 0);
+            if (Build.VERSION.SDK_INT <= 21)
+                dots[i].setImageDrawable(context.getResources().getDrawable(R.drawable.normal_circle_item));
+            else
+                dots[i].setImageDrawable(context.getResources().getDrawable(R.drawable.normal_circle_item, null));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(0, 0, 0, 0);
+
+            dotsLinearLayout.addView(dots[i], params);
+        }
+
+        if (Build.VERSION.SDK_INT <= 21)
+            dots[0].setImageDrawable(context.getResources().getDrawable(R.drawable.selected_circle_item));
+        else
+            dots[0].setImageDrawable(context.getResources().getDrawable(R.drawable.selected_circle_item, null));
+    }
+}
