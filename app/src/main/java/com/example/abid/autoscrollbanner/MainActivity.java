9502e51ce44bb09b6a57f43cba7dad5016e22dea
abidTss <abid.teastallstudio@gmail.com>
2018-08-01 15:01:40 +0530

    gradle update

--- app/src/main/java/com/example/abid/autoscrollbanner/MainActivity.java
+++ app/src/main/java/com/example/abid/autoscrollbanner/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.abid.autoscrollbanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
 
-import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
